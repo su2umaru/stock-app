3f2d87707be348c04a741ad4b3d2de566438e251
su2umaru <ckl15076@nitech.jp>
2021-03-21 13:51:36 +0900

    prepare sample data

---
+++ utils/sample-data.ts
@@ -0,0 +1,8 @@
+import { App } from '../interfaces'
+
+export const sampleAppData: App[] = [
+  { id: 1, name: 'Google Chrome' },
+  { id: 2, name: 'Alfred' },
+  { id: 3, name: 'Karabiner-Elements' },
+  { id: 4, name: 'Rectangle' },
+]
